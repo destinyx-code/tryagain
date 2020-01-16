2db5e32849e3b1f4ec4a4b364563ca301036946d
Destiny <59299839+destinyx-code@users.noreply.github.com>
2020-01-15 17:51:08 -0700

    Update code.cs

--- code.cs
+++ code.cs
@@ -5,14 +5,10 @@ namespace tryagain;
   class Program;
   
     static void Main(string[] args)
-    
+    {
+      }
       Console.WriteLine("Choose a number between 1 and 10!");
   
         double correctNumber = 7;
-          
-          
-      
-      
-    
-      
+       
 }
