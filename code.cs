84ae07e7dccfd5a282ff184e42d3aaf94a66ef0c
Destiny <ryderstanley2007@gmail.com>
2020-01-19 10:35:10 -0700

    Update code.cs

--- code.cs
+++ code.cs
@@ -1,14 +1,26 @@
 using System;
 
-namespace tryagain;
-{
-  class Program;
-  
-    static void Main(string[] args)
-    {
-      }
-      Console.WriteLine("Choose a number between 1 and 10!");
-  
-        double correctNumber = 7;
-       
-}
+namespace code;
+
+	class Program;
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Pick a number between 1 - 10!");
+			
+				int answer = 7;
+				
+					string userAnswer = Console.ReadLine();
+					
+						if (userAnswer = answer)
+						{
+							Console.WriteLine("Correct!");
+							
+								else
+								{
+									Console.WriteLine("Try Again!");
+									
+								}
+						}
+		}
+	}
